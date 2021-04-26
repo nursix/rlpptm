aac92813f911b942df22a0d727e80b8a0b268c23
Dominic König <dominic@nursix.org>
2021-04-26 10:23:23 +0200

    Disable site services for now

--- modules/templates/RLPPTM/config.py
+++ modules/templates/RLPPTM/config.py
@@ -2161,6 +2161,21 @@ def config(settings):
                                     hidden = True,
                                     ),
                     ])
+            site_services = S3WithIntro(
+                                S3SQLInlineLink(
+                                    "service",
+                                    label = T("Services"),
+                                    field = "service_id",
+                                    widget = "groupedopts",
+                                    cols = 1,
+                                    ),
+                                intro = ("org",
+                                         "facility",
+                                         "SiteServiceIntro",
+                                         ),
+                                )
+        else:
+            site_services = None
 
         # Custom CRUD form
         crud_fields = [#"organisation_id",
@@ -2177,19 +2192,7 @@ def config(settings):
                        (T("Telephone"), "phone1"),
                        "email",
                        (T("Opening Hours"), "opening_times"),
-                       S3WithIntro(
-                            S3SQLInlineLink(
-                                "service",
-                                label = T("Services"),
-                                field = "service_id",
-                                widget = "groupedopts",
-                                cols = 1,
-                                ),
-                            intro = ("org",
-                                     "facility",
-                                     "SiteServiceIntro",
-                                     ),
-                       ),
+                       site_services,
                        "comments",
                        #"obsolete",
                        ]
