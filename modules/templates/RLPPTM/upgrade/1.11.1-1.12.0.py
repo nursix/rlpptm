57c9538d838eae24585524186f335fbf35ced441
Dominic König <dominic@nursix.org>
2021-08-16 22:34:37 +0200

    Release RLPPTM-1.12.0

---
+++ modules/templates/RLPPTM/upgrade/1.11.1-1.12.0.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+#
+# Database upgrade script
+#
+# RLPPTM Template Version 1.11.1 => 1.12.0
+#
+# Execute in web2py folder after code upgrade like:
+# python web2py.py -S eden -M -R applications/eden/modules/templates/RLPPTM/upgrade/1.11.1-1.12.0.py
+#
+import sys
+
+#from gluon.storage import Storage
+#from gluon.tools import callback
+from s3 import S3Duplicate
+
+# Override auth (disables all permission checks)
+auth.override = True
+
+# Failed-flag
+failed = False
+
+# Info
+def info(msg):
+    sys.stderr.write("%s" % msg)
+def infoln(msg):
+    sys.stderr.write("%s\n" % msg)
+
+# Load models for tables
+#otable = s3db.org_organisation
+
+IMPORT_XSLT_FOLDER = os.path.join(request.folder, "static", "formats", "s3csv")
+TEMPLATE_FOLDER = os.path.join(request.folder, "modules", "templates", "RLPPTM")
+
+# -----------------------------------------------------------------------------
+# Deploy task to poll for DCC requests
+#
+if not failed:
+    info("Deploy task to poll for DCC requests")
+
+    # Initially deploy with a 20sec interval
+    # - shorter intervalls means fewer DCC requests per cycle,
+    #   however, we need system resources for other tasks too
+    task_id = current.s3task.schedule_task("settings_task",
+                                           ["poll_dcc"],
+                                           {},
+                                           repeats = 0,
+                                           period = 20,
+                                           timeout = 60,
+                                           )
+    if task_id:
+        infoln("...done (task ID %s)" % task_id)
+    else:
+        failed = True
+        infoln("failed!")
+
+# -----------------------------------------------------------------------------
+# Deploy new CMS items
+#
+if not failed:
+    info("Deploy new CMS items")
+
+    # File and Stylesheet Paths
+    stylesheet = os.path.join(IMPORT_XSLT_FOLDER, "cms", "post.xsl")
+    filename = os.path.join(TEMPLATE_FOLDER, "cms_post.csv")
+
+    # Only import relevant CMS posts, do not update any existing ones
+    def cms_post_duplicate(item):
+        name = item.data.get("name")
+        if name in ("HomepageIntroDisabled",
+                    ):
+            S3Duplicate(noupdate=True)(item)
+        else:
+            item.skip = True
+
+    # Import, fail on any errors
+    try:
+        with open(filename, "r") as File:
+            resource = s3db.resource("cms_post")
+            resource.configure(deduplicate = cms_post_duplicate)
+            resource.import_xml(File,
+                                format = "csv",
+                                stylesheet = stylesheet,
+                                )
+    except:
+        infoln("...failed")
+        infoln(sys.exc_info()[1])
+        failed = True
+    else:
+        if resource.error:
+            infoln("...failed")
+            infoln(resource.error)
+            failed = True
+        else:
+            infoln("...done")
+
+# -----------------------------------------------------------------------------
+# Finishing up
+#
+if failed:
+    db.rollback()
+    infoln("UPGRADE FAILED - Action rolled back.")
+else:
+    db.commit()
+    infoln("UPGRADE SUCCESSFUL.")
