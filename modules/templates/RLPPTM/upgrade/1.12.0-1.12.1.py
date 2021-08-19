2c288e574255741fcbe533cd1d9c3fd2bccfb17b
Dominic König <dominic@nursix.org>
2021-08-19 10:01:28 +0200

    Release RLPPTM-1.12.1

---
+++ modules/templates/RLPPTM/upgrade/1.12.0-1.12.1.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#
+# Database upgrade script
+#
+# RLPPTM Template Version 1.12.0 => 1.12.1
+#
+# Execute in web2py folder after code upgrade like:
+# python web2py.py -S eden -M -R applications/eden/modules/templates/RLPPTM/upgrade/1.12.0-1.12.1.py
+#
+import sys
+
+#from gluon.storage import Storage
+#from gluon.tools import callback
+#from s3 import S3Duplicate
+
+# Override auth (disables all permission checks)
+auth.override = True
+
+# Failed-flag
+failed = False
+
+# Info
+def info(msg):
+    sys.stderr.write("%s" % msg)
+def infoln(msg):
+    sys.stderr.write("%s\n" % msg)
+
+# Load models for tables
+#otable = s3db.org_organisation
+
+IMPORT_XSLT_FOLDER = os.path.join(request.folder, "static", "formats", "s3csv")
+TEMPLATE_FOLDER = os.path.join(request.folder, "modules", "templates", "RLPPTM")
+
+# -----------------------------------------------------------------------------
+# Establish indexes for permission table
+#
+if not failed:
+    info("Add indexes to permissions table")
+
+    auth.permission.create_indexes()
+    infoln("...done")
+
+# -----------------------------------------------------------------------------
+# Finishing up
+#
+if failed:
+    db.rollback()
+    infoln("UPGRADE FAILED - Action rolled back.")
+else:
+    db.commit()
+    infoln("UPGRADE SUCCESSFUL.")
